0ac952b9364533cbc12ad14fa0bc49878841db05
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-11-09 20:49:34 +0100

    regroupement des polluants

--- concentration_mensuelle.py
+++ concentration_mensuelle.py
@@ -6,11 +6,6 @@ from IPython.display import HTML
 from IPython.display import display
 import numpy as np
 
-url='https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_mensuelle_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=nom_dept,nom_com,nom_poll,valeur,date_debut&outSR=4326&f=json'
-
-import requests
-import pandas as pd
-
 url = 'https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_mensuelle_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=nom_dept,nom_com,nom_poll,valeur,date_debut&outSR=4326&f=json'
 response = requests.get(url)
 
@@ -29,11 +24,19 @@ if response.status_code == 200:
     # Création d'un DataFrame
     df_data = pd.DataFrame(records)
 
-    # Remplacer les valeurs manquantes par "-" pour toutes les colonnes
-    df_data = df_data.fillna('-')
+    #Supprimer les lignes avec des données manquantes
+    df_data=df_data.dropna()
+    
+    # Convertir la colonne 'date_debut' en format datetime si ce n'est pas déjà fait
+    df_data['date_debut'] = pd.to_datetime(df_data['date_debut'], errors='coerce')
+
+    # Grouper les valeurs de chaque polluant 
+    grouped_data = df_data.groupby('nom_poll')['valeur'].agg(list).reset_index()
 
     # Affichage du DataFrame
     pd.set_option('display.max_rows', 10)  # Ajustez le nombre de lignes affichées selon vos besoins
     display(df_data)
+
+    print(grouped_data)
     
 
