6788b23ab20af9c33559b3d4d345b536b677ac81
Arthur Tena <arthur.tena@etu.umontpellier.fr>
2023-11-17 10:35:00 +0100

    ajouts des df par an et par dept

--- mesure_anuelle_PP.py
+++ mesure_anuelle_PP.py
@@ -5,3 +5,27 @@ import numpy as np
 data=pd.read_csv("/Users/arthurtena/Documents/data/Mesure_annuelle_Region_Occitanie_Polluants_Principaux.csv")
 df=pd.DataFrame(data)
 columns_to_drop=['code_station','typologie','influence','id_poll_ue', 'unite', 'metrique', 'date_fin', 'statut_valid' ]
+df=df.drop(columns=columns_to_drop)
+print(df)
+
+
+# Créer un DataFrame par département
+#df_departements = {}
+#for dept, group in df.groupby('nom_dept'):
+# df_departements[dept] = group
+    # Imprimer les DataFrames pour chaque département
+# for dept, df_dept in df_departements.items():
+#     print(f"\nDataFrame pour le département {dept} :")
+#     print(df_dept)
+
+# Créer un DataFrame par an
+df_annee = {}
+
+# Grouper par 'date_debut'
+for annee, group in df.groupby('date_debut'):
+    df_annee[annee] = group
+
+# Imprimer les DataFrames pour chaque année
+for annee, df_annee in df_annee.items():
+    print(f"\nDataFrame pour l'année {annee} :")
+    print(df_annee)
