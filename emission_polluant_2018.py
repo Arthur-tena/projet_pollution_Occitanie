62b9ba5a16bc72f63e3f1660a1a65878dcb2bcee
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-11-17 10:00:10 +0100

    recup donnees 72h

--- emission_polluant_2018.py
+++ emission_polluant_2018.py
@@ -1,87 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import requests
 
-# Charger les données de pollution à partir d'un fichier CSV
-data = pd.read_csv(r'C:\Users\aicha\Downloads\emission_polluant_2018.csv')
+url='https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_72h_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=nom_dept,nom_station,code_station,nom_poll,id_poll_ue,valeur,unite,date_debut,x_l93,y_l93&outSR=4326&f=json'
 
-# Afficher les premières lignes du DataFrame pour vérifier les données
-print(data.head())
+response = requests.get(url)
 
-# Afficher les noms des colonnes
-column_names = data.columns
-print(column_names)
+if response.status_code == 200:
+    data = response.json()
 
-# Supprimer plusieurs colonnes
-columns_to_drop = ['ESRI_OID', 'lib_epci','version','annee_inv','SHAPE_Length','SHAPE_Area','ges_teqco2']
-data = data.drop(columns=columns_to_drop)
-print(data)
+    # Extraction des entités de la réponse JSON
+    features = data.get('features', [])
 
-"""# Sélectionner les colonnes à utiliser pour le tracé
-data_to_plot = data[['date_ech', 'val_no2', 'val_o3', 'val_pm25']]
+    # Extraction des données pertinentes de chaque entité
+    records = []
+    for feature in features:
+        attributes = feature.get('attributes', {})
+        records.append(attributes)
 
-# Convertir la colonne "date_ech" en format de date (si ce n'est pas déjà le cas)
-data_to_plot['date_ech'] = pd.to_datetime(data_to_plot['date_ech'])
+    # Création d'un DataFrame
+    df_data = pd.DataFrame(records)
 
-# Regrouper les données par mois et calculer la moyenne
-data_grouped = data_to_plot.groupby(data_to_plot['date_ech'].dt.to_period('M')).mean()
+    #Supprimer les lignes avec des données manquantes
+    df_data=df_data.dropna()
+else:
+    print("Erreur")
 
-# Tracer un graphique à barres
-plt.figure(figsize=(4, 5))  # Facultatif : définir la taille du graphique
-plt.bar(data_grouped.index.strftime('%b %Y'), data_grouped['val_no2'], label='val_no2')
-plt.bar(data_grouped.index.strftime('%b %Y'), data_grouped['val_o3'], label='val_o3')
-plt.bar(data_grouped.index.strftime('%b %Y'), data_grouped['val_pm25'], label='val_pm25')
-bottom=data_grouped['val_no2'] + data_grouped['val_o3']
-plt.xlabel('Mois')
-plt.ylabel('Valeurs moyennes')
-plt.title('Qualité de l\'air en fonction du mois')
-plt.xticks(rotation=45)
-plt.legend()
-
-# Afficher le graphique
-plt.show()"""
-
-
-
-"""import pandas as pd
-import plotly.express as px
-from plotly.offline import plot
-import numpy as np
-x = np.arange(13)
-bdd = pd.read_csv("/home/qufst/projetgroupe2/GroupProject/basededonnee2020/donnees2020.csv")
-bdd2 = bdd[bdd['code_zone'] == 200070803]
-bdd2['coleur']='haute pyrenees'
-bdd3 = bdd[bdd['code_zone'] == 200042372]
-bdd3['coleur']='gers'
-bdd4 = bdd[bdd['code_zone'] == 248200099]
-bdd4['coleur']='tarn et garonne'
-bdd5 = bdd[bdd['code_zone'] == 244600573]
-bdd5['coleur']='lot'
-bdd6 = bdd[bdd['code_zone'] == 241200187]
-bdd6['coleur']='aveyron'
-bdd7 = bdd[bdd['code_zone'] == 241200914]
-bdd7['coleur']='tarn'
-bdd8 = bdd[bdd['code_zone'] == 243100518]
-bdd8['coleur']='haute garonne'
-bdd9 = bdd[bdd['code_zone'] == 200066223]
-bdd9['coleur']='ariege'
-bdd10 = bdd[bdd['code_zone'] == 200069144]
-bdd10['coleur']='lozere'
-bdd11 = bdd[bdd['code_zone'] == 200034692]
-bdd11['coleur']='gard'
-bdd12 = bdd[bdd['code_zone'] == 243400819]
-bdd12['coleur']='herault'
-bdd13 = bdd[bdd['code_zone'] == 200035715]
-bdd13['coleur']='aude'
-bdd14 = bdd[bdd['code_zone'] == 200027183]
-bdd14['coleur']='pyrenees orientales'
-
-BDD = pd.concat([bdd2, bdd3, bdd4, bdd5, bdd6, bdd7, bdd8, bdd9, bdd10, bdd11, bdd12, bdd13, bdd14], ignore_index=True)
-
-
-fig = px.scatter(BDD, x='date_ech', y='val_no2', animation_frame='date_ech',animation_group='code_zone', color='coleur',size = 'val_no2',hover_name='date_ech',log_x=False,range_x=(0,355),range_y=(-1,5))
-#fig = px.line(BDD, x='date_ech', y='val_no2', animation_frame='date_ech', animation_group='code_zone', color='coleur', line_shape='linear')
-fig.update_layout(
-    title='évolution du no2 en stade en occitanie durant2020',
-    xaxis_title='2020',
-    yaxis_title='Stade de pollution')
-fig.show()"""
+print(df_data)
