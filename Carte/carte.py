a4818d324ef817f28df77af151b617b1fc8f387d
Arthur Tena <arthur.tena@etu.umontpellier.fr>
2023-11-18 13:41:13 +0100

    modification code carte (essaie qui ne marche pas

--- Carte/carte.py
+++ Carte/carte.py
@@ -1,16 +1,8 @@
 import pygal_maps_fr
-from pygal_maps_fr import Regions
 from pygal.style import LightColorizedStyle
-
-# Créer une carte de l'Occitanie
-occitanie_map = pygal_maps_fr.Regions(style=LightColorizedStyle)
-occitanie_map.title = 'Carte de l\'Occitanie'
-
-# Ajouter la région Occitanie
-occitanie_map.add('Occitanie', ['oc'])
-
-# Enregistrer la carte dans un fichier (optionnel)
-#occitanie_map.render_to_file('carte_occitanie.svg')
-
-# Afficher la carte dans une fenêtre (optionnel)
-occitanie_map.render_in_browser()
+fr_chart = pygal_maps_fr.Regions()
+fr_chart.title = 'Some regions'
+fr_chart.add('Métropole', ['82', '11', '93'])
+fr_chart.add('Corse', ['94'])
+fr_chart.add('DOM COM', ['01', '02', '03', '04'])
+fr_chart.render()
