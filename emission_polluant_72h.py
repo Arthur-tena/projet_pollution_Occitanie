fd59dbc6eeef1783241c8e4651ac8f0011586510
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-11-19 18:28:44 +0100

    changement date reste a voir comment regroupe les données par départements

--- emission_polluant_72h.py
+++ emission_polluant_72h.py
@@ -4,7 +4,7 @@ import requests
 from pyproj import Proj, transform
 from datetime import datetime, timedelta
 
-url='https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_72h_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=nom_dept,nom_station,code_station,nom_poll,valeur,unite,date_debut,date_fin,x_l93,y_l93&outSR=4326&f=json'
+url='https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/mesures_occitanie_72h_poll_princ/FeatureServer/0/query?where=1%3D1&outFields=nom_dept,nom_station,code_station,nom_poll,valeur,unite,date_debut,x_l93,y_l93&outSR=4326&f=json'
 
 response = requests.get(url)
 
@@ -32,12 +32,7 @@ if response.status_code == 200:
     df_data['longitude'], df_data['latitude'] = transform(in_proj, out_proj, df_data['x_l93'].values, df_data['y_l93'].values)
     
     print(df_data.tail()) 
-    #reste a voir comment on va les gérer pour les faires apparaitre sur la carte
 
-    #Afficher seulement les colonnes 'date_ech' et 'date_fin'
-    selected_columns = ['date_debut', 'date_fin']
-    df_selected = df_data[selected_columns]
-    print(df_selected)
     # Supposons que vous avez une colonne 'date_debut' avec des durées en millisecondes
     df_data['date_debut'] = pd.to_datetime(df_data['date_debut'], unit='ms')
 
@@ -52,41 +47,56 @@ if response.status_code == 200:
     for dept, df_dept in df_departements.items():
         print(f"\nDataFrame pour le département {dept} :")
         print(df_dept)
-
-    """# Supposons que vous avez une colonne 'date_debut' avec des durées en millisecondes
-    df_data['date_debut'] = pd.to_datetime(df_data['date_debut'], unit='ms')
-
-    # Extraire les composants jour, mois, année et heure
-    df_data['jour'] = df_data['date_debut'].dt.day
-    df_data['mois'] = df_data['date_debut'].dt.month
-    df_data['annee'] = df_data['date_debut'].dt.year
-    df_data['heure'] = df_data['date_debut'].dt.hour
-
-    # Imprimer les résultats
-    print(df_data[['date_debut', 'jour', 'mois', 'annee', 'heure']])"""
-
-    """# Supposons que vous avez une durée en millisecondes
-    duree_millisecondes = df_data['date_debut']
-    # Convertir les millisecondes en secondes
-    duree_secondes = duree_millisecondes / 1000
-    # Utiliser timedelta pour créer un objet de durée
-    duree_timedelta = timedelta(seconds=duree_secondes)
-
-    # Utiliser un objet de référence pour calculer la date de départ
-    date_reference = datetime(2023, 11, 11)
-    date_resultat = date_reference + duree_timedelta
-
-    # Imprimer le résultat
-    print(date_resultat)"""
-
-    # Créer un DataFrame par département
-    df_departements = {}
+    
+    
+    
+    """df_departements = {}
     for dept, group in df_data.groupby('nom_dept'):
         df_departements[dept] = group
     # Imprimer les DataFrames pour chaque département
     for dept, df_dept in df_departements.items():
         print(f"\nDataFrame pour le département {dept} :")
-        print(df_dept)
+        print(df_dept)"""
+
+        # Créer une colonne 'polluant_valeur' en combinant le nom du polluant et la valeur
+        #df_data['polluant_valeur'] = df_data['nom_poll'] + '_' + df_data['valeur'].astype(str)
+
+        # Créer un DataFrame par département en regroupant par la colonne 'polluant_valeur'
+    # Créer un DataFrame par département
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    """df_departements = {}
+    for dept, group in df_data.groupby('nom_dept'):
+        # Regrouper les polluants dans chaque DataFrame de département
+        df_dept_polluants = group.groupby('nom_poll').agg({'unite': 'first'}).reset_index()
+        df_departements[dept] = df_dept_polluants
+
+        # Imprimer les DataFrames pour chaque département
+        print(f"\nDataFrame pour le département {dept} avec les polluants regroupés :")
+        print(df_dept_polluants)"""
 
 
 
--- indice_qualite_air_auj.py
+++ indice_qualite_air_auj.py
@@ -2,8 +2,8 @@ import pandas as pd
 import requests
 
 
-url='https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/Indice_quotidien_de_qualité_de_lair_pour_les_collectivités_territoriales_en_Occitanie/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
-esponse = requests.get(url)
+url='https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/Indice_quotidien_de_qualité_de_l’air_pour_les_collectivités_territoriales_en_Occitanie/FeatureServer/0/query?where=1%3D1&outFields=date_ech,code_qual,lib_qual,code_zone,code_no2,code_so2,code_o3,code_pm10,code_pm25,x_wgs84,y_wgs84,x_reg,y_reg,coul_qual&outSR=4326&f=json'
+response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
@@ -22,6 +22,12 @@ if response.status_code == 200:
 
     #Supprimer les lignes avec des données manquantes
     df_data=df_data.dropna()
+
+    # Supposons que vous avez une colonne 'date_debut' avec des durées en millisecondes
+    df_data['date_ech'] = pd.to_datetime(df_data['date_ech'], unit='ms')
+
+    # Extraire les composants jour, mois, année et heure
+    #df_data['date_formatee'] = df_data['date_ech'].dt.strftime('%d/%m/%Y %H:%M')
 
     print(df_data)
 
