b65a95233df8a079195ea9e8993d7f6aa1355ccf
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-11-19 21:28:22 +0100

    changement date reste a voir comment regroupe les données par départements

--- indice_qualite_air_auj.py
+++ indice_qualite_air_auj.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import requests
+import geopandas as gpd
 
 
 url='https://services9.arcgis.com/7Sr9Ek9c1QTKmbwr/arcgis/rest/services/Indice_quotidien_de_qualité_de_l’air_pour_les_collectivités_territoriales_en_Occitanie/FeatureServer/0/query?where=1%3D1&outFields=date_ech,code_qual,lib_qual,code_zone,code_no2,code_so2,code_o3,code_pm10,code_pm25,x_wgs84,y_wgs84,x_reg,y_reg,coul_qual&outSR=4326&f=json'
@@ -25,9 +26,34 @@ if response.status_code == 200:
 
     # Supposons que vous avez une colonne 'date_debut' avec des durées en millisecondes
     df_data['date_ech'] = pd.to_datetime(df_data['date_ech'], unit='ms')
-
     # Extraire les composants jour, mois, année et heure
-    #df_data['date_formatee'] = df_data['date_ech'].dt.strftime('%d/%m/%Y %H:%M')
+    df_data['date_formatee'] = df_data['date_ech'].dt.strftime('%d/%m/%Y')
 
     print(df_data)
+
+    # Créer un GeoDataFrame à partir du DataFrame avec les données
+    gdf_data = gpd.GeoDataFrame(df_data, geometry=gpd.points_from_xy(df_data['x_wgs84'], df_data['y_wgs84']), crs='EPSG:4326')
+
+    # Charger les données des départements
+    departements = gpd.read_file('data/departements_occitanie.geojson')
+
+    # Fusion des deux GeoDataFrames
+    result = gpd.sjoin(gdf_data, departements, how='inner', op='within')
+
+    # Afficher le résultat
+    print(result)
+
+    """# Créer un GeoDataFrame à partir du DataFrame avec les données
+    gdf_data = gpd.GeoDataFrame(df_data, geometry=gpd.points_from_xy(df_data['x_wgs84'], df_data['y_wgs84']),crs='EPSG:4326')
+
+    #Charger les données des départements 
+    departements = gpd.read_file('data/departements_occitanie.geojson')
+
+    #Fusion des deux GeoDataFrames
+    result = gpd.sjoin(gdf_data, departements, how='inner', op='within')
+
+    # Afficher le résultat
+    print(gdf_data)"""
+
+   
 
