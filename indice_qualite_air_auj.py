f701215adc8521f65bf7934f664cc1d03b9e1eab
aichalhj <aicha.lahjiouj@etu.umontpellier.fr>
2023-11-26 20:07:36 +0100

    création dataframe par departement

--- indice_qualite_air_auj.py
+++ indice_qualite_air_auj.py
@@ -30,32 +30,47 @@ if response.status_code == 200:
     df_data['date_formatee'] = df_data['date_ech'].dt.strftime('%d/%m/%Y')
 
     print(df_data)
+    #c:/Users/aicha/OneDrive/Bureau/Projet pollution Occitanie/projet_pollution_Occitanie/data
+    # Lecture d'un csv contenant tout les codes zone associées aux 13 départements
+    dept = pd.read_excel("data/dept.xlsx")
 
-   # Ariège
-    df_Ariège = df_data[df_data['code_zone'].isin(data['ariège'])]
+    # Get  13 clean (i.e doesn't contain empty values) lists that contains the codes of zones of each departments from our data frame "dept"
+    dept_lists = [dept[col].dropna().tolist() for col in dept.columns]
+
+    # From the "dept" df get the names of each department 
+    dept_names = dept.columns.to_list()
+
+    # Create a dictionary that assign each dept to it's codes of zone   
+    data2= dict(zip(dept_names,dept_lists))
+    print("Names of departments from data:", data.keys())
+    print("Names of departments from data2:", data2.keys())
+    
+    # Ariège
+    df_Ariège = df_data[df_data['code_zone'].isin(data2['Ariège'])]
     # Aude
-    df_Aude = df_data[df_data['code_zone'].isin(data['Aude'])]
+    df_Aude = df_data[df_data['code_zone'].isin(data2['Aude'])]
     # Averyron
-    df_Averyron = df_data[df_data['code_zone'].isin(data['Averyron'])]
+    df_Averyron = df_data[df_data['code_zone'].isin(data2['Averyron'])]
     # Gard
-    df_Gard = df_data[df_data['code_zone'].isin(data['Gard'])]
+    df_Gard = df_data[df_data['code_zone'].isin(data2['Gard'])]
     # Gers 
-    df_Gers = df_data[df_data['code_zone'].isin(data['Gers'])]
+    df_Gers = df_data[df_data['code_zone'].isin(data2['Gers'])]
     # Hautes_Pyrénées
-    df_Hautes_Pyrénées = df_data[df_data['code_zone'].isin(data['Hautes-Pyrénées'])]
+    df_Hautes_Pyrénées = df_data[df_data['code_zone'].isin(data2['Hautes-Pyrénées'])]
     # Hérlaut
-    df_Hérlaut = df_data[df_data['code_zone'].isin(data['Hérlaut'])]
+    df_Hérlaut = df_data[df_data['code_zone'].isin(data2['Hérlaut'])]
     #Lot
-    df_Lot = df_data[df_data['code_zone'].isin(data['Lot'])]
+    df_Lot = df_data[df_data['code_zone'].isin(data2['Lot'])]
     # Lozère
-    df_Lozère = df_data[df_data['code_zone'].isin(data['Lozère'])]
+    df_Lozère = df_data[df_data['code_zone'].isin(data2['Lozère'])]
     # Pyrénées_Orientales
-    df_Pyrénées_Orientales = df_data[df_data['code_zone'].isin(data['Pyrénées-Orientales'])]
+    df_Pyrénées_Orientales = df_data[df_data['code_zone'].isin(data2['Pyrénées-Orientales'])]
     # Tarn
-    df_Tarn = df_data[df_data['code_zone'].isin(data['Tarn'])]
+    df_Tarn = df_data[df_data['code_zone'].isin(data2['Tarn'])]
     # Tarn_garonne
-    df_Tarn_garonne = df_data[df_data['code_zone'].isin(data['Tarn-et garonne'])]
+    df_Tarn_garonne = df_data[df_data['code_zone'].isin(data2['Tarn-et garonne'])]
     # Haute_Garonne
-    df_Haute_Garonne = df_data[df_data['code_zone'].isin(data['la Haute-Garonne'])]
-   
+    df_Haute_Garonne = df_data[df_data['code_zone'].isin(data2['Haute-Garonne'])]
+
+    
 
